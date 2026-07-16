5ef0dad02f733585b227cedcb4633aa3087dc931
Mickaël K <contact.aeryle@pm.me>
2026-07-17 01:49:04 +0200

    :speech_balloon: rename UserEvent into MentionPrefixEvent

--- src/listeners/mentionPrefixOnly.ts
+++ src/listeners/mentionPrefixOnly.ts
@@ -2,7 +2,7 @@ import type { Events } from '@sapphire/framework'
 import { Listener } from '@sapphire/framework'
 import type { Message } from 'discord.js'
 
-export class UserEvent extends Listener<typeof Events.MentionPrefixOnly> {
+export class MentionPrefixEvent extends Listener<typeof Events.MentionPrefixOnly> {
   public override run(message: Message) {
     // Do nothing if we cannot send messages in the channel (eg. group DMs)
     if (!message.channel.isSendable()) return
